2be3558cfbdfb826e4dbf287b09a05954e6c0a14
XanJan <annamajberger@gmail.com>
2024-10-21 00:01:42 +0200

    Started designing GenImages Page

--- capstone/src/Components/GenImages.js
+++ capstone/src/Components/GenImages.js
@@ -1,8 +1,30 @@
 import React from "react";
+import Header from "./Header";
+import { List } from "reactstrap";
 
 function GenImages(){
     return (
-        <div>Hello</div>
+        <div>
+            <Header />
+            <h1 className="design-text">From vision to reality - generate <b>your</b> room</h1>
+            <hr width="70%" />
+            <h2>Design</h2>
+
+            <div className="options">
+            <label for="roomType">Type of room:</label>
+            <select id="roomType" name="roomType">
+                <option value="blank" selected disabled hidden>Select room</option>
+                <option value="bedroom">Bedroom</option>
+                <option value="dorm">Dorm room</option>
+                <option value="studio">Studio Apartment</option>
+            </select>
+
+            <label for="roomSize">Type of room:</label>
+            <input type="text" id="size" name="size"></input>
+            
+            </div>
+
+        </div>
     );
 }
 
