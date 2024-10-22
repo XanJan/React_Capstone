2642457f64c8ba035ee1b88f5904a9ec398a127f
XanJan <annamajberger@gmail.com>
2024-10-22 23:42:33 +0200

    Changes

--- capstone/src/Components/PopupWindow.js
+++ capstone/src/Components/PopupWindow.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./PopupWindow.css"
+import { CiSearch } from "react-icons/ci";
 
 function PopupWindow(props){
     return(props.trigger) ? (
         <div className="popup">
-            <button className="close-button">X</button>
+            <button className="close-button"></button>
             {props.children}
-            <input type="text" placeholder="Search.." className="inputSearch"></input>
+            
+            <div className="input-bar">
+            <input type="text" placeholder="Search.." className="input-search"></input>
+            <button className="input-search-button" type="submit"><CiSearch className="icon"/></button>
+            </div>
+            <hr width="100%" />
 
             <div className="inner-popup">
                 
